50490a69b038735a6e8096740644b57b95c67126
Kurt Pili <kurtpili3@gmail.com>
2022-02-16 15:02:04 -0600

    Applied validation to product model and tested validation using seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Seed new data every time seed script is run.
+Product.delete_all
